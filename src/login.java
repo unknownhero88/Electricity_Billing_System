115c581360633db3f5799ce30663b146634ea51d
Rishi Sahu <rishisahu8822@gmail.com>
2026-06-04 16:41:15 +0530

    Improve login security and user experience

--- src/login.java
+++ src/login.java
@@ -12,7 +12,7 @@ public class login extends JFrame implements ActionListener
     JTextField tf1;
     JPasswordField pf2;
     JButton b1,b2;
-    JPanel p1,p2,p3,p4;
+    JPanel p2,p4;
 
     login()
     {
@@ -23,27 +23,27 @@ public class login extends JFrame implements ActionListener
         pf2=new JPasswordField(15);
 
         ImageIcon ic1=new ImageIcon(ClassLoader.getSystemResource("images/login.jpg"));
-        Image i1=ic1.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
+        Image i1=ic1.getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH);
         b1=new JButton("Login",new ImageIcon(i1));
 
         ImageIcon ic2=new ImageIcon(ClassLoader.getSystemResource("images/cancel.png"));
-        Image i2=ic2.getImage().getScaledInstance(50,50,Image.SCALE_DEFAULT);
+        Image i2=ic2.getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH);
         b2=new JButton("Cancel",new ImageIcon(i2));
 
         b1.addActionListener(this);
         b2.addActionListener(this);
 
         ImageIcon ic3=new ImageIcon(ClassLoader.getSystemResource("images/pop.jpg"));
-        Image i3=ic3.getImage().getScaledInstance(340,370,Image.SCALE_DEFAULT);
+        Image i3=ic3.getImage().getScaledInstance(340,370,Image.SCALE_SMOOTH);
         ImageIcon icc3=new ImageIcon(i3);
 
         l3=new JLabel(icc3);
 
         setLayout(new BorderLayout());
 
-        p1=new JPanel();
+
         p2=new JPanel();
-        p3=new JPanel();
+
         p4=new JPanel();
 
         add(l3,BorderLayout.WEST);
@@ -65,28 +65,46 @@ public class login extends JFrame implements ActionListener
         setLocation(600,400);
         setVisible(true);
 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 
     public void actionPerformed(ActionEvent ae){
 
+        if(ae.getSource() == b2){
+            dispose();
+            return;
+        }
+
         try{
             conn c1 = new conn();
-            String a  = tf1.getText();
-            String b  = pf2.getText();
-            String q  = "select * from login where username = '"+a+"' and password = '"+b+"'";
-            ResultSet rs = c1.s.executeQuery(q);
+            String Username  = tf1.getText();
+            String Password =  String.valueOf(pf2.getPassword());
+            /*String q  = "select * from login where username = '"+a+"' and password = '"+b+"'";*/
+
+            PreparedStatement ps = c1.c.prepareStatement("SELECT * from login where userName=? and password=?");
+            ps.setString(1,Username);
+            ps.setString(2,Password);
+            ResultSet rs = ps.executeQuery();
+
             if(rs.next()){
                 new Project().setVisible(true);
-                this.setVisible(false);
-
+                dispose();
             }else{
-                JOptionPane.showMessageDialog(null, "Invalid login");
-                setVisible(false);
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Invalid username or password."
+                );
+
+                pf2.setText("");
             }
         }catch(Exception e){
-            e.printStackTrace();
-            System.out.println("error: "+e);
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Login failed.\n" + e.getMessage(),
+                    "Database Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
         }
     }
 
